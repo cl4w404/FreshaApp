4aeaa1264620c1b7fe6e5cbbdbea52e4bc4fd5b8
cl4w404 <mbuguangigi254@gmail.com>
2024-01-24 11:31:19 +0300

    first commit

---
+++ src/test/java/com/devcorp/freshi/FreshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcorp.freshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
